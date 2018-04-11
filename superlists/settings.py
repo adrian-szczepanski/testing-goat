a4b0123243137f364dda172a89483478ad48bfd5
Adrian Szczepański <4szczepanski@gmail.com>
2018-04-11 23:49:35 +0200

    Set STATIC_ROOT in settings and disable admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -33,7 +33,7 @@ TEST_RUNNER='green.djangorunner.DjangoRunner'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
