23654ab17fd2c3674ac1ee9eed98a6edf7f8d64c
Adrian Szczepański <kontakt@aszczepanski.pl>
2018-07-12 00:07:32 +0200

    Use env vars for prod settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,13 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jlbeg=-w@sm66wo0p*lclhq+m*5)^pz1dzl!i(k(^6zqtlo_er'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+# Environment-specific settings
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'jlbeg=-w@sm66wo0p*lclhq+m*5)^pz1dzl!i(k(^6zqtlo_er'
+    ALLOWED_HOSTS = ['*']
 
 # Test runner
 TEST_RUNNER='green.djangorunner.DjangoRunner'
